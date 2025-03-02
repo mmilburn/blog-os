70001ff71ae1179206522b400790ab4d83f9db2a
Mark Milburn <mark.a.milburn@gmail.com>
2025-03-01 17:09:54 -0700

    Setup macros for VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
@@ -14,6 +15,7 @@ static HELLO: &[u8] = b"Hello, World!";
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
     loop {}
 }
